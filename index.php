c70410fad327763576f969df868f614ee33e7687
Roman Taranenko <roman@aruodas.lt>
2016-10-21 15:12:19 +0300

    Non console logic

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
-$container = require __DIR__ . '/app/bootstrap.php';
-$container->get('console')->run();
+if (!empty($argv)) {
+    $container = require __DIR__ . '/app/bootstrap.php';
+    $container->get('console')->run();
+} else {
+    echo 'It\'s a console app (╯°□°）╯︵ ┻━┻ ';
+}
